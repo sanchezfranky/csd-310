e00c9fb7f61f0a48ebfcc758a2df9818c4a7f727
sanchezfranky <fsanchez@my365.bellevue.edu>
2023-12-03 19:13:58 -0600

    commit attempt 12/3/2023 commit message for your changes. Lines starting.

---
+++ module_6/mysql_test.py
@@ -0,0 +1,12 @@
+# import mysql.connector
+import mysql.connector
+
+# Connect to the database
+mydb = mysql.connector.connect(
+    host='localhost',
+    user='root',
+    password='LKSaua327*69',
+    database='movies')
+
+
+print(mydb)
